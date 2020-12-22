69a6be65c1abc0e6a63524e42a9bba4449f9c154
Krzysztof Lipczak <01026898@pw.edu.pl>
2020-12-22 22:49:41 +0100

    Revert "Test commit"
    This reverts commit 1cd90a454a2691cd90d009503ab2e61451170e5b.

--- EdgeDetectionPoc/Program.cs
+++ EdgeDetectionPoc/Program.cs
@@ -12,7 +12,6 @@ namespace EdgeDetectionPoc
     {
         static void Main(string[] args)
         {
-            //aaaa
             var path = @"c:\seki\Sek2.png";
             var savepath = @"c:\seki\Canny2.png";
 
